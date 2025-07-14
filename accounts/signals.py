39b481acebd7ac49369c800f80b38b4dec6ebc2c
andrwxy <andrewparedesbravoxd@gmail.com>
2025-07-13 22:09:52 -0500

    Implementacion de la Biblioteca para cada usuario nuevo

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,8 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User, Perfil
+from biblioteca.models import Biblioteca
 
 @receiver(post_save, sender=User)
 def crear_perfil_usuario(sender, instance, created, **kwargs):
     if created:
         Perfil.objects.create(user=instance)
+        Biblioteca.objects.create(user=instance)
